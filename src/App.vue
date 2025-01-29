59ed8175a1b0def61c21206cc1183ebb77c9c3ff
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2025-01-29 18:22:17 +0000

    fix: remove default vue logo

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,26 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <RouterLink to="/" class="logo-wrapper">
 
-    <div class="wrapper">
+      <img alt="Sampurn logo" class="logo" src="@/assets/sampurn-logo.png" width="140px"/>
+    </RouterLink>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+    <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> -->
+    <nav>
+      <RouterLink to="/" class="path">Home</RouterLink>
+      <RouterLink to="#about" >About Us</RouterLink>
+      <RouterLink to="/course" class="path">Course</RouterLink>
+      <RouterLink to="#faq" >FAQs</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
@@ -24,22 +34,24 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
+  padding: 1rem 3rem;
+  /* background: #74210F; */
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  /* margin: 0 auto 2rem; */
 }
 
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+  /* margin-top: 2rem; */
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+nav a.router-link-exact-active.path {
+  color: red;
 }
 
 nav a.router-link-exact-active:hover {
@@ -49,37 +61,42 @@ nav a.router-link-exact-active:hover {
 nav a {
   display: inline-block;
   padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  color: var(--cream);
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: 500
+
+  /* border-left: 1px solid var(--color-border); */
 }
 
 nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+@media (min-width: 900px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    padding-top: 2rem;
+    /* place-items: center; */
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #FF7200;
+    /* padding-right: calc(var(--section-gap) / 2); */
+
   }
 
   .logo {
-    margin: 0 2rem 0 0;
+    /* margin: 0 2rem 0 0; */
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
+    text-align: right;
+    /* margin-left: -1rem; */
     font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+    /* padding: 1rem 0; */
+    /* margin-top: 1rem; */
   }
 }
 </style>
