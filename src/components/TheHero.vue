7fe5e936976ee6065f5507983b4bc39ce40ebcb4
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2025-01-29 18:17:57 +0000

    feat: implement hero section

---
+++ src/components/TheHero.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="wrapper">
+    <img src="@/assets/sampurn-logo.png" alt="Sampurn's logo" width="300px" />
+    <p>Master Trading: Your Journey to Financial Journey starts here</p>
+    <button>ENROL NOW</button>
+  </section>
+</template>
+
+<style scoped>
+.wrapper {
+  min-height: 100vh;
+  background: purple;
+}
+</style>
