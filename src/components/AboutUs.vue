450a627a8edc646cabf55765233823a1cec8b3c0
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2025-01-29 18:19:50 +0000

    feat: implement about us section

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MatTemplate from './MatTemplate.vue'
+</script>
+
+<template>
+  <MatTemplate subHeading="Our Mission and Vision" heading="About Us">
+    <p class="mat-essay">
+      At Sampurn, our mission is to empower aspiring and experienced traders to achieve financial
+      independence through a deep understanding of Financial markets.<br /><br />
+      Our vision is to create a global community of confident traders who leverage cutting-edge
+      tools and strategies to succeed in a rapidly changing financial landscape. Over the years,
+      Sampurn has trained over 200 successful traders. Many of whom have gone on to build thriving
+      trading careers. From industry accolades to student testimonials, our track record speaks for
+      itself.
+    </p>
+  </MatTemplate>
+</template>
