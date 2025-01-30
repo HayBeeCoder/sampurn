70afe09ac006a57bd11b8cb190c6e594ed37fa58
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2025-01-30 17:23:06 +0000

    feat: update faqs content

--- src/components/OurFaqs.vue
+++ src/components/OurFaqs.vue
@@ -9,29 +9,51 @@ type FaqItem = {
 }
 const Faqs: FaqItem[] = [
   {
-    display: 'Does registrationasdajsdkfjasd safljkdfdf asdfkflasdfk;j asfdkskadlj start on the 1st of Feb or 25th?',
-    details:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia nihil explicabo placeat ex accusantium dolorum sapiente modi illo maiores esse.',
+    display: 'Does registration start on the 1st of Feb or 15th?',
+    details: 'Registration (Payment) starts on the 15th of February.',
   },
   {
-    display: 'Does registration start on the 1st of Feb or 25th?',
+    display: 'How can one join the waitlist to pay just $100?',
     details:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia nihil explicabo placeat ex accusantium dolorum sapiente modi illo maiores esse.',
+      'By 5pm on the 1st of Feb, a link to a telegram group will be posted on my WhatsApp status. The group has been automated to accept only 100 people. Those first 100 are the ones paying $100. Every other person will access Sampurn for $150.',
   },
   {
-    display: 'Does registration start on the 1st of Feb or 25th?',
-    details:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia nihil explicabo placeat ex accusantium dolorum sapiente modi illo maiores esse.',
+    display: 'Will there be extra tutors for Sampurn?',
+    details: 'No. It’s just me.',
   },
   {
-    display: 'Does registration start on the 1st of Feb or 25th?',
+    display: 'Can a complete beginner with no prior knowledge of trading join Sampurn?',
     details:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia nihil explicabo placeat ex accusantium dolorum sapiente modi illo maiores esse.',
+      'Yes. Sampurn starts from scratch, from 0. Someone who has never even placed a trade before is safe to join.',
   },
   {
-    display: 'Does registration start on the 1st of Feb or 25th?',
+    display: 'How long is the mentorship access and will we have full access to you?',
     details:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia nihil explicabo placeat ex accusantium dolorum sapiente modi illo maiores esse.',
+      'The mentorship lasts for one year. During this one year, your growth is my utmost priority. You have complete access to me.',
+  },
+  {
+    display: 'How long will the period of tutorship last before we start executing?',
+    details:
+      'Sampurn covers crypto, forex, and synthetics courses. This means that we have to cover a lot, so the period of tutorship may extend to 1.5 - 2 months.',
+  },
+  {
+    display: 'Can students or 9-5ers also pay for Sampurn?',
+    details:
+      'Yes. I’m a medical student myself, so I understand how tight student schedules can be. The timing will be comfortable enough for people in these shoes.',
+  },
+  {
+    display:
+      'Since the waitlist will accommodate only 100 people, don’t you think people will join that day, fill it up without an intent to pay, thereby blocking others from getting in?',
+    details: 'Everybody that gets on the waitlist will be screened before registration starts.',
+  },
+  {
+    display: 'Will stacking at specific points be taught?',
+    details:
+      'I have always been an advocate of not stacking randomly. So yes, Sampurn students will be taught to do it right.',
+  },
+  {
+    display: 'Will you teach how you passed your prop firm challenges?',
+    details: 'Yes.',
   },
 ]
 
@@ -92,7 +114,9 @@ function onClickFaq(faqNumber: number) {
   transform: scaleY(0);
   transform-origin: top;
   opacity: 0;
-  transition: transform 0.5s ease-in-out, opacity 0.1s ease-in-out;
+  transition:
+    transform 0.5s ease-in-out,
+    opacity 0.1s ease-in-out;
   top: -2px;
   position: absolute;
 }
@@ -100,11 +124,11 @@ function onClickFaq(faqNumber: number) {
 .show-faq-details {
   opacity: 1;
   transform: scaleY(1);
-  transition: transform 0.5s ease-in-out, opacity 0.3s ease-in-out;
+  transition:
+    transform 0.5s ease-in-out,
+    opacity 0.3s ease-in-out;
   position: relative;
 }
-
-
 
 @media (min-width: 369px) {
 }
@@ -112,7 +136,7 @@ function onClickFaq(faqNumber: number) {
 }
 
 @media (min-width: 969px) {
-  .faq-list{
+  .faq-list {
     max-width: 80%;
   }
 }
