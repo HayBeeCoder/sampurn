02e8ac62b5d26c25af148530c8f46c657b42631b
HayBeeCoder <haybeecodes@gmail.com>
2025-01-31 10:40:21 +0000

    fix: each section paddings

--- src/components/MatTemplate.vue
+++ src/components/MatTemplate.vue
@@ -23,7 +23,7 @@ const props = defineProps<{
   background-repeat: no-repeat;
   /* background-position: 100% 100%; */
   background-size: 100% 100%;
-  padding: 5rem 0.2em;
+  padding: 5rem 2em;
   
   text-align: center;
   position: relative;
