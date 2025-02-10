87690e94cfc06e50650e13b36ad293d79e684623
HayBeeCoder <designerytg@skiff.com>
2025-02-10 02:16:07 +0100

    [feature] make button clickable

--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -1,31 +1,34 @@
-<template>
+<script lang="ts" setup>
 
-    <button class="btn hero-btn btn-default-font yellow-orange-gradient shiny">
-        <p class="btn-mask-gradient">
-            <slot></slot>
-        </p>
-    </button>
+</script>
+<template>
+  <button class="btn hero-btn btn-default-font yellow-orange-gradient shiny" @click="$emit('click', $event)">
+    <p class="btn-mask-gradient">
+      <slot></slot>
+    </p>
+  </button>
 </template>
 
 <style scoped>
 .btn p {
-    font-size: 1rem;
-    font-weight: 700;
+  font-size: 1rem;
+  font-weight: 700;
 }
 
 .btn {
-    padding: 0.8em 2em;
-    border-radius: 0.6em;
-    border: 1px solid #FEEA15
+  padding: 0.8em 2em;
+  border-radius: 0.6em;
+  border: 1px solid #feea15;
+  cursor: pointer;
 }
 
 .hero-btn {
-    margin-top: 2em;
+  margin-top: 2em;
 }
 
 .btn-default-font {
-    text-transform: uppercase;
-    font-weight: 600;
-    font-size: 1.4rem;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 1.4rem;
 }
 </style>
