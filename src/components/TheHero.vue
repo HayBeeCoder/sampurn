394c1f9e57acd7be8f7a0ac193aac7b90c928eff
HayBeeCoder <designerytg@skiff.com>
2025-02-13 22:35:32 +0100

    [feature] apply animation to hero items

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,16 +1,83 @@
 <script lang="ts" setup>
-import TheButton from "@/components/TheButton.vue"
+import { onMounted, ref } from 'vue';
+import TheButton from '../components/TheButton.vue'
+defineProps<{
+  toggleOnModal: () => void;
+
+}>()
+
+
+
+const showElements = ref(false)
+
+onMounted(() => {
+  showElements.value = true
+})
 </script>
 
 <template>
   <section class="hero-gradient wrapper">
-    <img src="@/assets/sampurn-logo.png" alt="Sampurn's logo" class="hero-logo" />
-    <p class="hero-tagline">Master Trading: Your Journey to Financial Freedom starts here</p>
-    <TheButton>Enrol Now</TheButton>
+    <transition name="logo">
+
+
+        <img src="@/assets/sampurn-logo.png" alt="Sampurn's logo" class="hero-logo" v-if="showElements" />
+
+    </transition>
+    <transition name="tagline">
+
+      <p class="hero-tagline" v-if="showElements">Master Trading: Your Journey to Financial Freedom starts here</p>
+    </transition>
+
+    <transition name="button">
+      <TheButton @click="toggleOnModal" v-if="showElements">Enrol Now</TheButton>
+    </transition>
   </section>
 </template>
 
 <style scoped>
+
+.logo-enter-from{
+  opacity: 0;
+  transform: translateY(-50vh);
+}
+
+.logo-enter-to{
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.logo-enter-active{
+  transition: all 1s;
+}
+
+.tagline-enter-from{
+  opacity: 0;
+  transform: translateY(50vh);
+}
+
+.tagline-enter-to{
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.tagline-enter-active{
+  transition: all 1s 1s;
+}
+
+.button-enter-from{
+  opacity: 0;
+  transform: translateY(50vh);
+}
+
+.button-enter-to{
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.button-enter-active{
+  transition: all 1s 1.2s;
+}
+
 .wrapper {
   overflow-x: hidden;
   min-height: 100vh;
@@ -37,7 +104,8 @@ import TheButton from "@/components/TheButton.vue"
   color: #d9d9d9;
 }
 
-@media (min-width: 369px) {}
+@media (min-width: 369px) {
+}
 
 @media (min-width: 768px) {
   .hero-logo {
@@ -45,12 +113,14 @@ import TheButton from "@/components/TheButton.vue"
   }
 
   .wrapper {
-    background-image:
-      linear-gradient(-180deg,
+    background-image: linear-gradient(
+        -180deg,
         hsla(0, 0%, 8%, 0.3) 0%,
         hsla(0, 0%, 6%, 0.6) 35%,
         hsla(0, 0%, 4%, 0.8) 65%,
-        hsla(0, 0%, 4%, 1) 90%), url('../assets/desktop-hero-sampurn-temple.png');
+        hsla(0, 0%, 4%, 1) 90%
+      ),
+      url('../assets/desktop-hero-sampurn-temple.png');
     background-repeat: no-repeat;
     background-position: -40%;
     background-size: 100%;
@@ -60,7 +130,6 @@ import TheButton from "@/components/TheButton.vue"
 }
 
 @media (min-width: 969px) {
-
   .hero-logo {
     width: 7;
   }
